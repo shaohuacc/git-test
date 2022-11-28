300d847b415b61325f03601556990418d015c85e
chc <shaohuacc@126.com>
2022-11-28 20:27:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/gitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author chc
+ * @description
+ * @create 2022/11/28-20:20
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,gitTest");
+    }
+}
