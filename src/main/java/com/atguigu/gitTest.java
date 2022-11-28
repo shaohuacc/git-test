87d834748a3c2a35b4355f1fbf986e1757cf3ade
chc <shaohuacc@126.com>
2022-11-28 21:45:25 +0800

    push commit

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -13,5 +13,6 @@ public class gitTest {
         System.out.println("hello,gitTest4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
